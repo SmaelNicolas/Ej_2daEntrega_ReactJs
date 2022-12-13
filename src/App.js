c8a0bf50210ba689a5304335cad0ea1864df146b
Smael Nicolas <nico.smael@gmail.com>
2022-12-13 15:13:24 -0300

    add BrowserRouter , create Routes , add elements to each routh , define paths

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
-	return <div className='container--app'></div>;
+	return (
+		<div className='container--app'>
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path='/' element={<ItemListContainer />} />
+					<Route
+						path='/category/:idCategory'
+						element={<ItemListContainer />}
+					/>
+					<Route
+						path='/items/:idItem'
+						element={<ItemDetailContainer />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
