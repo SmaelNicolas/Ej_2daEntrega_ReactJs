02c52f31808fab851f128062cfc47a268fa0aa56
Smael Nicolas <nico.smael@gmail.com>
2023-01-25 18:37:46 -0300

    updated commets

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,26 +4,34 @@ import "./navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className='container--navbar'>
+		<nav className="container--navbar">
 			{/* LOGO */}
-			<Link to='/' className='navbar--logo'>
+			<Link to="/" className="navbar--logo">
 				🪐
 			</Link>
 
 			{/* Secciones de la navbar */}
-			<ul className='navbar--ul'>
-				<li className='navbar--ul--li'>
-					<Link to='/category/keyboard' className='navbar--ul--li--a'>
+			<ul className="navbar--ul">
+				<li className="navbar--ul--li">
+					{/* UTILIZAMOS Link o NavLink DE REACT ROUTER DOM    */}
+					{/* EN to="" DEBEMOS PONER LA URL QUE CONFIGURAMOS EN APP  */}
+					{/* EN ESTE CASO EL VALOR DINAMICO 🚩 :idCategory 🚩 TOMA EL VALOR keyboard */}
+					{/* ESTE VALOR FIJO QUE TOMA EL DINAMICO DEBE COINCIDIR CON LAS CATEGORIAS QUE TENEMOS EN NUESTROS PRODUCTOS */}
+					<Link to="/category/keyboard" className="navbar--ul--li--a">
 						Keyboards
 					</Link>
 				</li>
-				<li className='navbar--ul--li'>
-					<Link to='/category/mouse' className='navbar--ul--li--a'>
+				<li className="navbar--ul--li">
+					{/* EN ESTE CASO EL VALOR DINAMICO 🚩 :idCategory 🚩 TOMA EL VALOR mouse */}
+					{/* ESTE VALOR FIJO QUE TOMA EL DINAMICO DEBE COINCIDIR CON LAS CATEGORIAS QUE TENEMOS EN NUESTROS PRODUCTOS */}
+					<Link to="/category/mouse" className="navbar--ul--li--a">
 						Mouses
 					</Link>
 				</li>
-				<li className='navbar--ul--li'>
-					<Link to='/category/pad' className='navbar--ul--li--a'>
+				<li className="navbar--ul--li">
+					{/* EN ESTE CASO EL VALOR DINAMICO 🚩 :idCategory 🚩 TOMA EL VALOR pad */}
+					{/* ESTE VALOR FIJO QUE TOMA EL DINAMICO DEBE COINCIDIR CON LAS CATEGORIAS QUE TENEMOS EN NUESTROS PRODUCTOS */}
+					<Link to="/category/pad" className="navbar--ul--li--a">
 						Pads
 					</Link>
 				</li>
